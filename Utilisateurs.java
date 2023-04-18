e3fbd9db58c855c8305180ffad70c82e8f04f6bc
SaifGtar <131154810+SaifGtar@users.noreply.github.com>
2023-04-18 16:19:40 +0200

    Add files via upload

---
+++ Utilisateurs.java
@@ -0,0 +1,20 @@
+package com.mycompany.projecttd2;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Utilisateurs {
+
+    /**
+     * Default constructor
+     */
+    public Utilisateurs() {
+    }
+
+    /**
+     * 
+     */
+    public String nom;
+
+}
